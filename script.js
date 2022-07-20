85900f83dbe8e9f7f3263f55ffa7899698d27502
Paulo Victorio <paulo_victorio@yahoo.com>
2022-07-20 18:35:28 +0800

    Add scoring capability

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const CHOICES = ["ROCK", "PAPER", "SCISSORS"];
+const score = {
+                playerScore: 0,
+                computerScore: 0,
+              }
 
 function getComputerChoice(choices) {
   return choices[Math.floor(Math.random() * 100) % 3];
@@ -8,15 +12,19 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection != computerSelection) {
     if (playerSelection == CHOICES[0] && computerSelection == CHOICES[2]) {
+      score.playerScore += 1;
       return "Your " + playerSelection + " had BEATEN the Computer's " + computerSelection  + "!";
     } 
     else if (playerSelection == CHOICES[1] && computerSelection == CHOICES[0]) {
+      score.playerScore += 1;
       return "Your " + playerSelection + " had BEATEN the Computer's " + computerSelection  + "!";
     } 
     else if (playerSelection == CHOICES[2] && computerSelection == CHOICES[1]) {
+      score.playerScore += 1;
       return "Your " + playerSelection + " had BEATEN the Computer's " + computerSelection  + "!";
     } 
     else {
+      score.computerScore += 1;
       return "Your " + playerSelection + " had LOST to the Computer's " + computerSelection  + "!";
     }    
   } 
@@ -37,6 +45,8 @@ function game() {
     
     console.log(playRound(playerSelection, computerSelection));
   }
+  console.log("Your score: " + score.playerScore);
+  console.log("Computer's score: " + score.computerScore);
 }
 
 game();
